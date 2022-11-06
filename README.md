bfc16082511ad393be2f24acbee061546832f387
Zijing's Hub <37041873+463548483@users.noreply.github.com>
2022-11-06 02:28:52 +0000

    proj 3

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Data Script with Databricks
+# SQL Script
 
 ![example workflow](https://github.com/nogibjj/Zijing-codespcase/actions/workflows/main.yml/badge.svg)
 
-This is Zijing's project 1 repo, which contains a big data script that use Databricks API and Streamlit app to show word cloud for job salary analysis. 
+This is Zijing's project 3 repo, which contains a sql script that 
 
 Demo link: https://github.com/nogibjj/Zijing-codespcase/blob/main/video3571322614.mp4
 
@@ -11,25 +11,18 @@ Dataset: https://www.kaggle.com/code/raghurayirath/plotly-data-science-job-salar
 ## Project Structure
 ![image](https://github.com/nogibjj/Zijing-codespcase/blob/main/structure.jpg)
 
-## Test CLI
-```linux
-databricks clusters list --output JSON | jq
-databricks fs ls dbfs:/
-databricks jobs list --output JSON | jq
-```
 
-## CLI Query 
-  `query-job-avg-salary`  Find job average salary
+## Main Feature
+### Convert CSV file to json file
+Convert ds_salaries.csv from Kaggle to json file to load into Dynamodb
+### Create Table in Dynamodb
+Create table salaries with schema
+### Insert data into Dynamodb
+Insert items into table
+### Database Query 
+Get first 5 items
+Query 2: Find jobs that pay more than $4000,000
+Query 3: Find data scientist job in US
+Query 4: Find job in small company
+Query 5: Find job salary between $100,000 and $200,000
 
-  `query-most-n`    --n      Find N most pay job, default=5
-
-```linux
-chmod +x query.py
-./query.py query-most-n --n 5 
-./query.py query-job-avg-salary
-```
-
-## Microservice
-```linux
-streamlit run app.py --server.enableCORS=false
-```
---
+++ querydb.py
@@ -0,0 +1,122 @@
+from logging import error
+from multiprocessing.context import set_spawning_popen
+import os
+from sre_parse import State
+import boto3, json
+import hashlib
+from botocore.exceptions import ClientError
+import pandas as pd
+
+# Step 1: Create JSON file from CSV
+def create_json_file():
+    df = pd.read_csv('ds_salaries.csv')
+    df.to_json('data.json')
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
+# Step 2: Create table
+def create_table():
+    table = dynamodb.create_table(
+        TableName='salaries',
+        KeySchema=[
+            {
+                'AttributeName': 'id',
+                'KeyType': 'HASH'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'id',
+                'AttributeType': 'N'
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5
+        }
+    )
+    table.meta.client.get_waiter('table_exists').wait(TableName='salaries')
+
+# Step 3: Load data into table
+def load_data():
+    table = dynamodb.Table('salaries')
+    print(table.table_status)
+    with open('data.json') as json_file:
+        data = json.load(json_file)
+        for p in data['id']:
+            id = data['id'][p]
+            employment_type = data['employment_type'][p]
+            title = data['job_title'][p]
+            location = data['company_location'][p]
+            salary = data['salary'][p]
+            company_size = data['company_size'][p]
+            print("Adding data:", id, employment_type, title, location, salary, company_size)
+            table.put_item(
+                Item={
+                    'id': id,
+                    'employment_type': employment_type,
+                    'title': title,
+                    'location': location,
+                    'salary': salary,
+                    'company_size': company_size,
+                }
+            )
+
+
+# create_json_file()
+# create_table()
+# load_data()
+
+# Step 4: Query data
+session = boto3.session.Session()
+client = session.client('dynamodb', region_name='us-east-1')
+
+## Query 1: Get first 5 items
+def query_first_five():
+    response = client.execute_statement(
+        Statement='SELECT * FROM salaries where id < 5',
+    )
+    # convert response to dataframe
+    df = pd.DataFrame(response['Items'])
+    print(df)
+## Query 2: Find jobs that pay more than $4000,000
+def query_salary():
+    response = client.execute_statement(
+        Statement='SELECT title, salary FROM salaries where salary > 4000000',
+    )
+    # convert response to dataframe
+    df = pd.DataFrame(response['Items'])
+    print(df)
+
+## Query 3: Find data scientist job in US
+def query_data_scientist_US():
+    response = client.execute_statement(
+        Statement='SELECT title, salary FROM salaries where title = "Data Scientist" and location = "US"',
+    )
+    # convert response to dataframe
+    df = pd.DataFrame(response['Items'])
+    print(df)
+
+## Query 4: Find job in small company
+def query_small_company():
+    response = client.execute_statement(
+        Statement='SELECT title, salary FROM salaries where company_size = "S" and salary > 1000000',
+    )
+    # convert response to dataframe
+    df = pd.DataFrame(response['Items'])
+    print(df)
+
+## Query 5: Find job salary between $100,000 and $200,000
+def query_salary_range():
+    response = client.execute_statement(
+        Statement='SELECT title, salary FROM salaries where salary between 100000 and 200000',
+    )
+    # convert response to dataframe
+    df = pd.DataFrame(response['Items'])
+    print(df)
+
+query_first_five()
+query_salary()
+query_data_scientist_US()
+query_small_company()
+query_salary_range()
